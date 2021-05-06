91633faa1a0fdb92b3dab2987e541b819fbd4428
Felipe Amador <felipedavidamador@gmail.com>
2021-05-06 11:23:04 -0300

    fix: fix urls

--- app/urls.py
+++ app/urls.py
@@ -5,15 +5,9 @@ from app.views import login, register, home, post, hometest
 app_name = "app"
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', home, name="home"),
     path('login/', login, name="login"),
     path('register/', register, name="register"),
-=======
-    path('login/', login, name="login"),
-    path('register/', register, name="register"),
-    path('home/', home, name="home"),
->>>>>>> develop
     path('post/', post, name="post"),
     path('hometest/', hometest, name="hometest")
 ]
